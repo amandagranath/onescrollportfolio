b11b8f99b3d408a1636d164b2a59c050abde9fe5
amandagranath <82177396+amandagranath@users.noreply.github.com>
2022-04-07 11:37:26 +0200

    Desktop

---
+++ js/main.js
@@ -0,0 +1,18 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3', 'section4'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#menu',
+    navigationTooltips: ['Home', 'About me', 'Portfolio', 'Contact'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+    responsiveWidth: 0,
+    responsiveHeight: 0,
+    lazyLoading: true,
+    cards: true,
+    controlArrows: false,
+    slidesNavigation: true
+
+
+})
